4a6a350cce77f7252b4a281493d2fb8b89f339da
Vojtech Benes <vojtech.benes@carvago.com>
2022-07-07 18:26:26 +0200

    add db checking

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const db = require('knex')({
 
 app.get('/db', async (req, res) => {
     try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM login');
+        const client = await db.connect();
+        const result = await client.query('SELECT * FROM test_table');
         const results = { 'results': (result) ? result.rows : null};
         res.render('pages/db', results );
         client.release();
