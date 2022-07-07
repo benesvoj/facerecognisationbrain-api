333ca9cb5681f8110576910b2c729208086491f6
Vojtech Benes <vojtech.benes@carvago.com>
2022-07-07 18:21:41 +0200

    add check for db connection

--- server.js
+++ server.js
@@ -23,6 +23,19 @@ const db = require('knex')({
     }
 });
 
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM login');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
+
 app.get('/', (req, res) => {
     res.send('it is working');
 });
